63c5bc213a474362ede6d404644ae1bcaab2cf9d
Timothy Roy <timothy.roy@protonmail.ch>
2021-04-14 21:02:32 -0500

    Started our testing suite with setup and sanity checks

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,15 @@
 // write tests here
+
+describe('Quotes App', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:1234')
+    })
+
+    it('sanity checks', () => {
+        expect(10).to.equal(10)
+        expect(1 + 2).to.equal(3)
+        expect({}).to.eql({}) // deep equals, comparing properties
+        expect(1 + 2).to.equal(4-1) // does this work?
+    })
+
+})
