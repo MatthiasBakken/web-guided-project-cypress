605b9c2ec7f5ac71e856729293a7edd844a61541
Timothy Roy <timothy.roy@protonmail.ch>
2021-04-14 22:00:43 -0500

    Tests typing inputs, submit button enabled, and cancel button

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -29,12 +29,39 @@ describe('Quotes App', () => {
     })
 
     describe('filling out inputs and cancelling', () => {
-        it('can type inside the inputs', () => {
+        it('can type inside the text input', () => {
             textInput()
             .should('have.value', '')
             .type('Be kind to yourself and others')
             .should('have.value', 'Be kind to yourself and others')
         })
+        // can type inside the author input
+        it('can type inside the author input', () => {
+            authorInput()
+            .should('have.value', '')
+            .type('Frank Herbert')
+            .should('have.value', 'Frank Herbert')
+        })
+
+        it('the submit button enables if we type into both inputs', () => {
+
+            submitButton().should('not.be.enabled')
+            // type in text input
+            textInput().type('foo')
+            // type in author input
+            authorInput().type('bar')
+            // select submit button --- ('not.be.disabled')
+            submitButton().should('be.enabled')
+        })
+
+        it("the cancel button can rest the inputs and disable the submit button", () => {
+            textInput().type("I like Chocolate Milk");
+            authorInput().type("Cheez");
+            cancelBtn().click();
+            textInput().should("have.value", "");
+            authorInput().should("have.value", "");
+            submitButton().should("be.disabled");
+          });
     })
 
 })
